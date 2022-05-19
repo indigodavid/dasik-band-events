434690a93d03922eceee56daa2ffc19cb8027539
David Vera <indigodavid@gmail.com>
2022-05-18 20:20:42 -0500

    Add Javascript to generate dynamically Speakers section

---
+++ js/speakers.js
@@ -0,0 +1,56 @@
+const speakers = document.getElementById('speakers');
+const speakerData = [
+  {
+    name: 'Daniel Vera',
+    role: 'Bass, main vocals, lyrics composer',
+    description: 'Dasik co-founder. Music & lyrics composer. Bass player. Producer. Leader of Dasik Band project',
+    image: './images/DanielVera.jpeg',
+  },
+  {
+    name: 'David Vera',
+    role: 'Guitar, backing vocals, music composer',
+    description: 'Dasik co-founder. Music composer. Guitar and bass player. Main arranger for the songs. Playing guitar since 2003',
+    image: './images/DavidVera.jpeg',
+  },
+  {
+    name: 'Damian Betancourt',
+    role: 'External consultant and touring musician',
+    description: 'Producer and touring musician. Multi-instrumentalist. Engineer. Music consultant',
+    image: './images/DamianBetancourt.jpeg',
+  },
+  {
+    name: 'Xavier De La Torre',
+    role: 'Touring musician. Drummer',
+    description: 'Drummer and percusionist. Touring musician.',
+    image: './images/XavierDeLaTorre.jpeg',
+  },
+];
+
+function generateLi(index) {
+  const li = document.createElement('li');
+  const photo = document.createElement('div');
+  const info = document.createElement('div');
+  const h3 = document.createElement('h3');
+  const h4 = document.createElement('h4');
+  const p = document.createElement('p');
+
+  photo.classList.add('photo');
+  info.classList.add('info');
+
+  photo.innerHTML = `<div class="pattern"> </div><img src="${speakerData[index].image}" alt="${speakerData[index].name}"></div>`;
+  h3.innerText = speakerData[index].name;
+  h4.innerText = speakerData[index].role;
+  p.innerHTML = speakerData[index].description;
+
+  info.appendChild(h3);
+  info.appendChild(h4);
+  info.appendChild(p);
+
+  li.appendChild(photo);
+  li.appendChild(info);
+  return li;
+}
+
+for(let i = 0; i < speakerData.length; i += 1) {
+  speakers.appendChild(generateLi(i));
+}
